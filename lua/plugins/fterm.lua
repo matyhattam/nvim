70c47c108532fe5f64e445721b6d2c29ef7ff9c1
maty hattam <maty.hattam@gmail.com>
2023-05-31 22:21:56 +0200

    fterm config

--- lua/plugins/fterm.lua
+++ lua/plugins/fterm.lua
@@ -1,18 +1,26 @@
 return {
-	"numToStr/FTerm.nvim",
-	keys = {
-		{ "`", '<CMD>lua require("FTerm").toggle()<CR>', desc = "Toggle terminal" },
-		{
-			"`",
-			'<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>',
-			mode = "t",
-			desc = "Toggle terminal",
-		},
-		-- {
-		--   "<ESC><ESC>",
-		--   "<C-\\><C-n>",
-		--   mode = "t",
-		--   desc = "Terminal normal mode",
-		-- },
-	},
+  "numToStr/FTerm.nvim",
+  config = function()
+    require("FTerm").setup({
+      dimensions = {
+        height = 0.85,
+        width = 0.85,
+      },
+    })
+  end,
+  keys = {
+    { "`", '<CMD>lua require("FTerm").toggle()<CR>', desc = "Toggle terminal" },
+    {
+      "`",
+      '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>',
+      mode = "t",
+      desc = "Toggle terminal",
+    },
+    -- {
+    --   "<ESC><ESC>",
+    --   "<C-\\><C-n>",
+    --   mode = "t",
+    --   desc = "Terminal normal mode",
+    -- },
+  },
 }
